e9721cad6fe0db787b3b06d784806c0fb36409af
LahiruHarshana <lharshana2002@gmail.com>
2023-12-17 00:39:56 +0530

    updated

--- src/main/java/lk/ijse/javaeethogakade/CustomerServlet.java
+++ src/main/java/lk/ijse/javaeethogakade/CustomerServlet.java
@@ -1,6 +1,11 @@
 package lk.ijse.javaeethogakade;
 
+import lk.ijse.javaeethogakade.db.DBConnection;
+
 import java.io.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
@@ -13,7 +18,19 @@ public class CustomerServlet extends HttpServlet {
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Connection connection = null;
+        try {
+            String id = request.getParameter("cusID");
+            String name = request.getParameter("cusName");
+            String address = request.getParameter("cusAddress");
+            String salary = request.getParameter("cusSalary");
 
+            connection = DBConnection.getDbConnection().getConnection();
+            PreparedStatement stm = connection.prepareStatement("INSERT INTO customer (cusID, cusName, cusAddress,cusSalary) VALUES (?, ?, ?,?)");
+
+        }catch (SQLException | ClassNotFoundException e){
+
+        }
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
--- src/main/java/lk/ijse/javaeethogakade/db/DBConnection.java
+++ src/main/java/lk/ijse/javaeethogakade/db/DBConnection.java
@@ -4,17 +4,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DBConnection {
-    private static DBConnection dbConnection;
-
+public class DBConnection {    private static DBConnection dbConnection;
     private final Connection connection;
 
-    private DBConnection() throws SQLException, ClassNotFoundException {
+    private DBConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
         connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/testThogaKade", "root", "12345678");
     }
+
     public static DBConnection getDbConnection() throws SQLException, ClassNotFoundException {
-        return (dbConnection==null)? dbConnection=new DBConnection() : dbConnection;
+        return dbConnection == null ? dbConnection= new DBConnection() : dbConnection;
+    }
+
+    public Connection getConnection() {
+        return connection;
     }
 
 }
