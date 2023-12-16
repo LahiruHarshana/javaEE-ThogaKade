75b48cd36ab7545f33ff4cd271ab50f0376bea7d
LahiruHarshana <lharshana2002@gmail.com>
2023-12-17 00:30:33 +0530

    updated

--- src/main/java/lk/ijse/javaeethogakade/ItemServlet.java
+++ src/main/java/lk/ijse/javaeethogakade/ItemServlet.java
@@ -1,8 +1,16 @@
 package lk.ijse.javaeethogakade;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(urlPatterns = "/item")
-public class ItemServlet {
+public class ItemServlet extends HttpServlet {
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+    }
 }
