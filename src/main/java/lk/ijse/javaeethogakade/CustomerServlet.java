6b0aa4c234064e90d859b13118e2882d48fa8f69
LahiruHarshana <lharshana2002@gmail.com>
2023-12-16 20:18:33 +0530

    updated

--- src/main/java/lk/ijse/javaeethogakade/CustomerServlet.java
+++ src/main/java/lk/ijse/javaeethogakade/CustomerServlet.java
@@ -14,14 +14,9 @@ public class CustomerServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-
-        // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
-    }
-
-    public void destroy() {
     }
 }
