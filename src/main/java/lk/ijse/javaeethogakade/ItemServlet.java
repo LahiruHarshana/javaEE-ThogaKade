443c816fd5838b5beccfd322b1db0980a47316c5
LahiruHarshana <lharshana2002@gmail.com>
2023-12-22 21:13:18 +0530

    updated

--- src/main/java/lk/ijse/javaeethogakade/ItemServlet.java
+++ src/main/java/lk/ijse/javaeethogakade/ItemServlet.java
@@ -1,11 +1,11 @@
 package lk.ijse.javaeethogakade;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import jakarta.servlet.ServletException;
+import lk.ijse.javaeethogakade.db.DBConnection;
+
+import java.io.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
 
 @WebServlet(urlPatterns = "/item")
 public class ItemServlet extends HttpServlet {
