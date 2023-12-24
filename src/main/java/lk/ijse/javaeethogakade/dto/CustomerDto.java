da53b3c8b72d2b44e454cc5e4fd6ce02962e2ade
LahiruHarshana <lharshana2002@gmail.com>
2023-12-24 22:59:53 +0530

    updated

--- src/main/java/lk/ijse/javaeethogakade/dto/CustomerDto.java
+++ src/main/java/lk/ijse/javaeethogakade/dto/CustomerDto.java
@@ -1,4 +1,9 @@
 package lk.ijse.javaeethogakade.dto;
 
 public class CustomerDto {
+        private String id;
+        private String name;
+        private String address;
+        private double salary;
 }
+
