6b9d16e6d42c1d4868ffb3bcb8e8f37d4940406b
LahiruHarshana <lharshana2002@gmail.com>
2023-12-24 23:00:54 +0530

    updated

--- src/main/java/lk/ijse/javaeethogakade/dto/CustomerDto.java
+++ src/main/java/lk/ijse/javaeethogakade/dto/CustomerDto.java
@@ -17,5 +17,37 @@ public class CustomerDto implements Serializable {
         this.address = address;
         this.salary = salary;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
 
