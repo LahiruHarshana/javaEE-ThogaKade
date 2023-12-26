46a776786e36ec00060f117b4b85dd4331b3314c
LahiruHarshana <lharshana2002@gmail.com>
2023-12-26 21:18:24 +0530

    updated

--- src/main/java/lk/ijse/javaeethogakade/CustomerServlet.java
+++ src/main/java/lk/ijse/javaeethogakade/CustomerServlet.java
@@ -79,5 +79,12 @@ public class CustomerServlet extends HttpServlet {
 
             ObjectMapper objectMapper = new ObjectMapper();
             CustomerDto customerDto = objectMapper.readValue(jsonInput.toString(), CustomerDto.class);
+
+            connection = DBConnection.getDbConnection().getConnection();
+            PreparedStatement stm = connection.prepareStatement("UPDATE customer SET cusName=?, cusAddress=?, cusSalary=? WHERE cusID=?");
+    } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
-}
