81d7ea86eab2943c487ffe3b573b7605138da1e6
LahiruHarshana <lharshana2002@gmail.com>
2023-12-30 21:02:20 +0530

    updated

--- src/main/java/lk/ijse/javaeethogakade/CustomerServlet.java
+++ src/main/java/lk/ijse/javaeethogakade/CustomerServlet.java
@@ -58,45 +58,42 @@ public class CustomerServlet extends HttpServlet {
 
         }
     }
+
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("application/json");
-
-        List<CustomerDto> customers = new ArrayList<>();
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Connection connection = null;
-
         try {
             connection = DBConnection.getDbConnection().getConnection();
-            PreparedStatement stm = connection.prepareStatement("SELECT * FROM customer");
-            ResultSet resultSet = stm.executeQuery();
+            PreparedStatement pstm = connection.prepareStatement("select * from Customer");
+            ResultSet rst = pstm.executeQuery();
+            PrintWriter writer = resp.getWriter();
+            resp.addHeader("Content-Type", "application/json");
+            resp.addHeader("Access-Control-Allow-Origin", "*");
 
-            while (resultSet.next()) {
-                CustomerDto customer = new CustomerDto(
-                        resultSet.getString("cusID"),
-                        resultSet.getString("cusName"),
-                        resultSet.getString("cusAddress"),
-                        resultSet.getDouble("cusSalary")
-                );
-                customers.add(customer);
+            JsonArrayBuilder allCustomer = Json.createArrayBuilder();
+
+            while (rst.next()) {
+                String id = rst.getString(1);
+                String name = rst.getString(2);
+                String address = rst.getString(3);
+                String salary = rst.getString(4);
+
+                JsonObjectBuilder customer = Json.createObjectBuilder();
+
+                customer.add("id", id);
+                customer.add("name", name);
+                customer.add("address", address);
+                customer.add("salary", salary);
+
+                allCustomer.add(customer.build());
             }
-        } catch (SQLException | ClassNotFoundException e) {
+            writer.print(allCustomer.build());
+
+        } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
         }
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        String json = objectMapper.writeValueAsString(customers);
-
-        PrintWriter out = response.getWriter();
-        out.println(json);
     }
+
 
 
     @Override
