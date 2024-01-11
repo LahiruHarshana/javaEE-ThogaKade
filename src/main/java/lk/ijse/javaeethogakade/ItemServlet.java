8daf373448d83a3d69cb020847f295e7243f6b62
LahiruHarshana <lharshana2002@gmail.com>
2024-01-11 22:14:50 +0530

    updated

--- src/main/java/lk/ijse/javaeethogakade/ItemServlet.java
+++ src/main/java/lk/ijse/javaeethogakade/ItemServlet.java
@@ -4,13 +4,33 @@ import jakarta.servlet.ServletException;
 import lk.ijse.javaeethogakade.db.DBConnection;
 
 import java.io.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 @WebServlet(urlPatterns = "/item")
 public class ItemServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Connection connection = null;
+        try {
+            connection = DBConnection.getDbConnection().getConnection();
+            PreparedStatement stm = connection.prepareStatement("INSERT INTO item (code, description, unitPrice,qtyOnHand) VALUES (?, ?, ?,?)");
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
     }
 }
