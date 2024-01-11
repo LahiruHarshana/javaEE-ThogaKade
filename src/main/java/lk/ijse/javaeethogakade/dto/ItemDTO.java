d335af6f6b6322e9f96f407b610b30b9661dba98
LahiruHarshana <lharshana2002@gmail.com>
2024-01-11 22:20:54 +0530

    updated

--- src/main/java/lk/ijse/javaeethogakade/dto/ItemDTO.java
+++ src/main/java/lk/ijse/javaeethogakade/dto/ItemDTO.java
@@ -1,4 +1,8 @@
 package lk.ijse.javaeethogakade.dto;
 
 public class ItemDTO {
+    private String code;
+    private String description;
+    private double unitPrice;
+    private int qtyOnHand;
 }
