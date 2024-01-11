140aa21b2ee42bbfa8c163b1716d6500e30fa629
LahiruHarshana <lharshana2002@gmail.com>
2024-01-11 22:22:06 +0530

    updated

--- src/main/java/lk/ijse/javaeethogakade/dto/ItemDTO.java
+++ src/main/java/lk/ijse/javaeethogakade/dto/ItemDTO.java
@@ -15,4 +15,36 @@ public class ItemDTO {
         this.unitPrice = unitPrice;
         this.qtyOnHand = qtyOnHand;
     }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getQtyOnHand() {
+        return qtyOnHand;
+    }
+
+    public void setQtyOnHand(int qtyOnHand) {
+        this.qtyOnHand = qtyOnHand;
+    }
 }
