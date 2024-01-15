767f6e2f32e04c1b8898fc12b39f7e23a3f17a5a
LahiruHarshana <lharshana2002@gmail.com>
2024-01-15 21:20:45 +0530

    updated

--- src/main/webapp/controllers/PurchaseOrderController.js
+++ src/main/webapp/controllers/PurchaseOrderController.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 
 function loadCustomerIds() {
     let selectId = $("#selectCustomerId");
-    selectId.empty(); // Clear existing options
+    selectId.empty();
 
     $.ajax({
         method: "GET",
